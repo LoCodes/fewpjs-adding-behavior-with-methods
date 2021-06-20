6e6d0582e26d8f9c3ba80f018606b60739cec86e
Loren Cipriano <loren25cipriano@gmail.com>
2021-06-20 10:51:44 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex){
+        this.name = name 
+        this.sex = sex
+    }
+
+    speak(){
+        return (`${this.name} says meow!`)
+    }
+}
+
+class Dog{
+    constructor(name, sex){
+        this.name = name 
+        this.sex = sex
+    }
+
+    speak(){
+        return (`${this.name} says woof!`)
+    }
+
+}
+
+
+class Bird{
+    constructor(name, sex){
+        this.name = name 
+        this.sex = sex
+    }
+
+    // practicing if else condition 
+
+    speak(){
+        if (this.sex === "male") {
+            return `It's me! ${this.name}, the parrot!`
+        }
+        else {
+            return `${this.name} says squawk!`
+        }
+    }
+    // speak () {
+    //     return (this.sex === 'male' ? `It's me! ${this.name}, the parrot!` : `${this.name} says squawk!`)
+    //   }
+
+}
